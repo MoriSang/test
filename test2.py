7d8bac9afe42c3f1e573c60c4b5681ee5b1f892a
Keithen <81213866+MoriSang@users.noreply.github.com>
2022-07-23 23:20:46 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ Created on 2022-7-23 19:12:08
 
 import re
 
-f = open('long_text.txt', 'r', encoding = "utf-8")
+f = open('long_text.txt', 'r', encoding="utf-8")
 content = f.readlines()
 content = ''.join(content).split("\n")
 f.close()
@@ -13,14 +13,19 @@ f.close()
 print('{')
 print('\t\'name\': \'{}\','.format(content[0]))
 print('\t\'lei\': \'{}\','.format(content[1]))
-print('\t\'sub_fund\': [{')
+print('\t\'sub_fund\': [', end="")
 sublist = content[2:]
-for i in sublist:
-    if i.find('.') != -1:
-        print(']\n\t}, {')
-        i = re.sub('[1-9.]', '', i)
-        print('\t\t\'title\': \'{}\','.format(i[1:]))
+for i in range(len(sublist)):
+    if sublist[i].find('.') != -1:
+        sublist[i] = re.sub('[1-9.]', '', sublist[i])
+        print('{'+'\n\t\t\'title\': \'{}\','.format(sublist[i][1:]))
         print('\t\t\'isin\': [', end="")
     else:
-        print('\'{}\', '.format(i), end="")
-print(']\n\t}]\n}')
+        print('\'{}\''.format(sublist[i]), end="")
+        if i+1 < len(sublist) and sublist[i+1][0] == 'L':
+            print(', ', end="")
+        elif i+1 == len(sublist):
+            print(']\n\t}]\n}')
+        else:
+            print(']\n\t}, ', end="")
+
